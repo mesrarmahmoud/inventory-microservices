1101b77350a19d597fc5c4d84edcb0aa8830f17b
mesrarmahmoud <122124412+mesrarmahmoud@users.noreply.github.com>
2024-07-28 18:56:04 +0100

    Delete tp-ebank-service directory

--- tp-ebank-service/src/main/java/org/sid/tp_ebank_service/enums/AccountType.java
+++
@@ -1,5 +0,0 @@
-package org.sid.tp_ebank_service.enums;
-
-public enum AccountType {
-    CURRENT_ACCOUNT, SAVING_ACCOUNT;
-}
--- tp-ebank-service/src/main/java/org/sid/tp_ebank_service/repositories/BankAccountRepository.java
+++
@@ -1,8 +0,0 @@
-package org.sid.tp_ebank_service.repositories;
-
-import org.sid.tp_ebank_service.entities.BankAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
-
-}
